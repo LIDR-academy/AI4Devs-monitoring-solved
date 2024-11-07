b34d1a760d9dc8fd7407bbc7b80852f926d5e06e
Pedro Avila <alejox31@gmail.com>
2024-11-06 23:58:36 -0500

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -9,7 +9,7 @@ terraform {
 
 # Configuración del proveedor de AWS
 provider "aws" {
-  region = "us-east-1" # Cambia a la región donde están tus instancias EC2
+  region = "us-east-2" # Cambia a la región donde están tus instancias EC2
 }
 
 # Configuración del proveedor de Datadog
@@ -24,11 +24,13 @@ provider "datadog" {
 variable "datadog_api_key" {
   description = "API Key para Datadog"
   type        = string
+
 }
 
 variable "datadog_app_key" {
   description = "App Key para Datadog"
   type        = string
+
 }
 
 # Política de IAM para permitir a Datadog acceder a CloudWatch
@@ -41,16 +43,107 @@ resource "aws_iam_policy" "datadog_policy" {
       {
         "Effect": "Allow",
         "Action": [
-          "cloudwatch:GetMetricData",
-          "cloudwatch:ListMetrics",
-          "ec2:DescribeInstances",
-          "logs:DescribeLogGroups",
-          "logs:DescribeLogStreams",
-          "logs:GetLogEvents",
-          "logs:FilterLogEvents",
-          "tag:GetResources",
-          "tag:GetTagKeys",
-          "tag:GetTagValues"
+"apigateway:GET",
+                "autoscaling:Describe*",
+                "backup:List*",
+                "backup:ListRecoveryPointsByBackupVault",
+                "bcm-data-exports:GetExport",
+                "bcm-data-exports:ListExports",
+                "budgets:ViewBudget",
+                "cassandra:Select",
+                "cloudfront:GetDistributionConfig",
+                "cloudfront:ListDistributions",
+                "cloudtrail:DescribeTrails",
+                "cloudtrail:GetTrailStatus",
+                "cloudtrail:LookupEvents",
+                "cloudwatch:Describe*",
+                "cloudwatch:Get*",
+                "cloudwatch:List*",
+                "codedeploy:BatchGet*",
+                "codedeploy:List*",
+                "cur:DescribeReportDefinitions",
+                "directconnect:Describe*",
+                "dynamodb:Describe*",
+                "dynamodb:List*",
+                "ec2:Describe*",
+                "ec2:GetSnapshotBlockPublicAccessState",
+                "ec2:GetTransitGatewayPrefixListReferences",
+                "ec2:SearchTransitGatewayRoutes",
+                "ecs:Describe*",
+                "ecs:List*",
+                "elasticache:Describe*",
+                "elasticache:List*",
+                "elasticfilesystem:DescribeAccessPoints",
+                "elasticfilesystem:DescribeFileSystems",
+                "elasticfilesystem:DescribeTags",
+                "elasticloadbalancing:Describe*",
+                "elasticmapreduce:Describe*",
+                "elasticmapreduce:List*",
+                "es:DescribeElasticsearchDomains",
+                "es:ListDomainNames",
+                "es:ListTags",
+                "events:CreateEventBus",
+                "fsx:DescribeFileSystems",
+                "fsx:ListTagsForResource",
+                "glacier:GetVaultNotifications",
+                "glue:ListRegistries",
+                "health:DescribeAffectedEntities",
+                "health:DescribeEventDetails",
+                "health:DescribeEvents",
+                "kinesis:Describe*",
+                "kinesis:List*",
+                "lambda:GetPolicy",
+                "lambda:List*",
+                "lightsail:GetInstancePortStates",
+                "logs:DeleteSubscriptionFilter",
+                "logs:DescribeLogGroups",
+                "logs:DescribeLogStreams",
+                "logs:DescribeSubscriptionFilters",
+                "logs:FilterLogEvents",
+                "logs:PutSubscriptionFilter",
+                "logs:TestMetricFilter",
+                "oam:ListAttachedLinks",
+                "oam:ListSinks",
+                "organizations:Describe*",
+                "organizations:List*",
+                "rds:Describe*",
+                "rds:List*",
+                "redshift:DescribeClusters",
+                "redshift:DescribeLoggingStatus",
+                "route53:List*",
+                "s3:GetBucketLocation",
+                "s3:GetBucketLogging",
+                "s3:GetBucketNotification",
+                "s3:GetBucketTagging",
+                "s3:ListAccessGrants",
+                "s3:ListAllMyBuckets",
+                "s3:PutBucketNotification",
+                "savingsplans:DescribeSavingsPlanRates",
+                "savingsplans:DescribeSavingsPlans",
+                "ses:Get*",
+                "sns:GetSubscriptionAttributes",
+                "sns:List*",
+                "sns:Publish",
+                "sqs:ListQueues",
+                "states:DescribeStateMachine",
+                "states:ListStateMachines",
+                "support:DescribeTrustedAdvisor*",
+                "support:RefreshTrustedAdvisorCheck",
+                "tag:GetResources",
+                "tag:GetTagKeys",
+                "tag:GetTagValues",
+                "timestream:DescribeEndpoints",
+                "waf-regional:ListRuleGroups",
+                "waf-regional:ListRules",
+                "waf:ListRuleGroups",
+                "waf:ListRules",
+                "wafv2:GetIPSet",
+                "wafv2:GetLoggingConfiguration",
+                "wafv2:GetRegexPatternSet",
+                "wafv2:GetRuleGroup",
+                "wafv2:ListLoggingConfigurations",
+                "xray:BatchGetTraces",
+                "xray:GetTraceSummaries"
         ],
         "Resource": "*"
       }
@@ -100,29 +193,3 @@ resource "datadog_dashboard" "ec2_dashboard" {
   }
 }
 
-
-
-# Crear una alerta en Datadog para el uso de CPU
-resource "datadog_monitor" "cpu_usage_alert" {
-  name               = "High CPU Utilization Alert"
-  type               = "metric alert"
-  message            = "La utilización de CPU ha superado el 80% durante más de 5 minutos. Por favor, revisa la instancia."
-  escalation_message = "La alerta de CPU sigue activa. Se requiere atención inmediata."
-
-  query = "avg(last_5m):avg:aws.ec2.cpuutilization{instance_id in (${join(",", data.aws_instances.all.ids)})} > 80"
-
-  # Configuración de notificaciones
-  notify_no_data = false
-  notify_audit   = false
-  timeout_h      = 0
-
-  # Configurar los umbrales del monitor
-  monitor_thresholds {
-    critical = 80
-  }
-
-  tags = [
-    "environment:production",
-    "team:operations"
-  ]
-}
